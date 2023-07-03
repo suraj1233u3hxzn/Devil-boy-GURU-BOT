9f6ff08b3aa90d6eb93b96747eb07d8121676f4d
suraj1233u3hxzn <138425157+suraj1233u3hxzn@users.noreply.github.com>
2023-07-03 10:20:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94712275423', 'me', true],
+  ['94712275423', 'devil x boy', true], 
   [''] 
 ] //Number of owners
 
